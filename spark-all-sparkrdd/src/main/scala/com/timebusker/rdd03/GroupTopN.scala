3208205edd4c8e3990c29d395398c7781e194cdf
timebusker <timebusker@gmail.com>
2019-07-22 22:05:14 +0800

    2019/07/22 22:05:14 ____ :sun_with_face::sunflower::palm_tree::house_with_garden::office::octocat::guitar::meat_on_bone:

--- spark-all-sparkrdd/src/main/scala/com/timebusker/rdd03/GroupTopN.scala
+++ spark-all-sparkrdd/src/main/scala/com/timebusker/rdd03/GroupTopN.scala
@@ -1,7 +1,10 @@
 package com.timebusker.rdd03
 
+import com.timebusker.rdd02.MostPopularTeacherTopN.resultPath
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
 
 /**
   * @DESC:GroupTopN： 需求：排出每个科目的前三名
@@ -26,21 +29,58 @@ object GroupTopN {
     val conf = new SparkConf().setAppName("GroupTopN").setMaster("local[8]")
     val sc = new SparkContext(conf)
 
-    val lineRDD = sc.textFile(inputPath, 3)
+    val linesRDD = sc.textFile(inputPath)
+    doGroupBykey(linesRDD)
   }
 
 
-  def doGroupBykey(lineRDD: RDD[String]) = {
-    val mapRDD = lineRDD.map(line => {
+  def doGroupBykey(linesRDD: RDD[String]) = {
+    val mapRDD = linesRDD.map(line => {
       var fields = line.split(" ")
-      (fields(0), (fields(1), fields(2)))
+      (fields(0), line)
     })
     // 进行shuffle
     val groupRDD = mapRDD.groupByKey()
+    groupRDD.foreach(println)
+    println("==========TopN前==========")
+    val resRDD = groupRDD.map(tuple => {
+      var set = new mutable.TreeSet[String]()(new MineOrdering())
+      val subject = tuple._1 //  chinese
+      val scores = tuple._2 //  ("ls 91", "ww 56", ...)
+      for (score <- scores) {
+        // 添加到treeSet中
+        set.add(score)
+        if (set.size > TOPN) {
+          // 如果大小大于3，则弹出最后一份成绩
+          set = set.dropRight(1)
+        }
+      }
+      (subject, set)
+    })
+    resRDD.saveAsTextFile(resultPath + "001")
 
+    val storeRDD = resRDD.map(x => {
+      (x._2.foreach(student => {
+        val subject = student.split(" ")(0)
+        val name = student.split(" ")(1)
+        val score = student.split(" ")(2)
+        println(subject, name, score)
+        (subject, name, score)
+      }))
+    })
+    storeRDD.saveAsTextFile(resultPath + "002")
   }
 
   def doCombineByKey(lineRDD: RDD[String]) = {
+    // https://blog.51cto.com/xpleaf/2108763
+  }
+}
 
+class MineOrdering extends Ordering[String] {
+  override def compare(x: String, y: String): Int = {
+    val xScore = x.split(" ")(2).toInt
+    val yScore = y.split(" ")(2).toInt
+    val ret = yScore - xScore
+    ret
   }
 }
